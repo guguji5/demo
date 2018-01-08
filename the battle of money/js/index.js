b73734a6c43e2068dce7335a680808f2b62929c3
guguji5 <604821528@qq.com>
2018-01-08 21:41:55 +0800

    it seem something is wrong.

--- the battle of money/js/index.js
+++ the battle of money/js/index.js
@@ -4,7 +4,7 @@
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
     recalc = function () {
     var clientWidth = docEl.clientWidth;
-    console.log(docEl.clientHeight);
+    console.log('the client height is ',docEl.clientHeight);
     if (!clientWidth) return;
     clientWidth=clientWidth>1033?1033:clientWidth;
     var rem = 100 * (clientWidth / 750);   //在宽度等于750px，1rem等于10px 当宽度增大的时候，随宽度增大
@@ -24,35 +24,41 @@
         this.a = a;
         this.position = position;
         this.id = id++;
-        this.time = 0
-        var template ='<img src="img/cloud.png" class="cloud">'+
+        this.time = 0;
+        this.template ='<img src="img/cloud.png" class="cloud">'+
             ' <img src="img/redlip0.png" class="iconandredlip">';
-
-        this.createNode = function () {
-            var container = document.querySelector(".game");
-            var divElement=document.createElement("div");
-            divElement.style.left = this.position;
-            divElement.setAttribute("id",this.id);
-            divElement.className='redlip lip'+this.style+' '+ this.size;
-            divElement.innerHTML=template;
-            container.appendChild(divElement);
-
-            var timer = setInterval(function (id) {
-                this.time+=1000;
-                // var length = (this.a*this.time*this.time)/2+this.v*this,time;
-                // var div = document.querySelector("#"+this.id);
-                console.log(id)
-            }(this.id),1000)
-        }
+    }
+    Redlip.prototype.createNode = function () {
+        var container = document.querySelector(".game");
+        var divElement=document.createElement("div");
+        divElement.style.left = this.position;
+        divElement.setAttribute("id","coin"+this.id);
+        divElement.className='redlip lip'+this.style+' '+ this.size;
+        divElement.innerHTML=this.template;
+        container.appendChild(divElement);
+        _self = this;
+        console.log("this id =",this.id,_self);
+        var timer = setInterval(function (id) {
+            console.log(_self)
+            _self.time+=1000;
+            var length = (_self.a*_self.time*_self.time)/2+_self.v*_self.time;
+            var div = document.querySelector("#"+"coin"+_self.id);
+            div.style.top = length+'px';
+        },1000);
     }
 
-    function createRedlip(style,size) {
+    function createRedlip(style,size,v,a) {
         var random = Math.ceil(20+Math.random()*70)+"%";
-        var redlip = new Redlip(style,size,1,1,random);
+        var redlip = new Redlip(style,size,v,a,random);
         redlip.createNode();
     }
 
-    createRedlip(1,'small');
-    createRedlip(2,'large');
+    createRedlip(2,'large',2,1);
+    createRedlip(1,'small',1,2);
+    // var random = Math.ceil(20+Math.random()*70)+"%";
+    // var redlip1 = new Redlip(1,'small',1,2,random);
+    // var redlip2 = new Redlip(2,'large',2,1,random);
+
+    // console.log(redlip1,redlip2)
 
 })(document, window);
