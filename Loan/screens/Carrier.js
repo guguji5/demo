af4cf39eb9e6054da1c14e01661c01b1f6ff8934
杜宽 <604821528@qq.com>
2018-04-24 22:06:19 +0800

    temporarily modify the webview uri

--- Loan/screens/Carrier.js
+++ Loan/screens/Carrier.js
@@ -18,7 +18,7 @@ export  default class CarrierScreen extends Component {
     render() {
         return (
             <WebView
-                source={{uri:"https://api.51datakey.com/h5/importV3/index.html#/carrier?apiKey=72a94c205c434107be76c5d1bf531259&userId=18903393333&backUrl=http%3A%2F%2F39.106.198.9%3A8080%2Fwebapi%2Fpages%2Fregister.html&themeColor=abcdef"}}
+                source={{uri:"https://ida.webank.com/s/web/h5/#/entry"}}
                 style={{marginTop: 0}}
                 onMessage={this.fromWeb}
             />
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 online demo
 ========
-
+<WebView
+                source={{uri:"https://api.51datakey.com/h5/importV3/index.html#/carrier?apiKey=72a94c205c434107be76c5d1bf531259&userId=18903393333&backUrl=http%3A%2F%2F39.106.198.9%3A8080%2Fwebapi%2Fpages%2Fregister.html&themeColor=abcdef"}}
+                style={{marginTop: 0}}
+                onMessage={this.fromWeb}
+            />
